5b3f122d81653ca1d9ad3736d132e1d3573234d3
manhhn01 <manhhn01@gmail.com>
2024-06-25 15:30:01 +0700

    chore: format

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,16 +1,14 @@
 return {
-  {
-    "akinsho/bufferline.nvim",
-    opts = {
-      options = {
-        always_show_bufferline = true,
-        indicator = {
-          -- style = "none",
-          icon = "▌",
-          style = "icon",
-        },
-        separator_style = { " ", " " },
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      always_show_bufferline = true,
+      indicator = {
+        -- style = "none",
+        icon = "▌",
+        style = "icon",
       },
+      separator_style = { " ", " " },
     },
   },
 }
