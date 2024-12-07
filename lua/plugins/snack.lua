cf49d8584d74900c5224208460a2806dd5695978
manhhn01 <manhhn01@gmail.com>
2024-12-07 22:11:09 +0700

    feat: disable dashboard

--- lua/plugins/snack.lua
+++ lua/plugins/snack.lua
@@ -19,65 +19,6 @@ return {
       win = {
         backdrop = 100, -- disable backdrop
       },
-      dashboard = {
-        width = 60,
-        row = nil, -- dashboard position. nil for center
-        col = nil, -- dashboard position. nil for center
-        pane_gap = 8, -- empty columns between vertical panes
-        autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
-        -- These settings are used by some built-in sections
-        preset = {
-          -- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
-          ---@type fun(cmd:string, opts:table)|nil
-          pick = nil,
-          -- Used by the `keys` section to show keymaps.
-          -- Set your custom keymaps here.
-          -- When using a function, the `items` argument are the default keymaps.
-          ---@type snacks.dashboard.Item[]
-          keys = {
-            { icon = " ", key = "n", desc = "New File", action = ":ene" },
-            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-            {
-              icon = " ",
-              key = "c",
-              desc = "Config",
-              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-            },
-            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-          },
-          -- Used by the `header` section
-          header = [[
-                                                                   
-      ████ ██████           █████      ██                 btw
-     ███████████             █████                            
-     █████████ ███████████████████ ███   ███████████  
-    █████████  ███    █████████████ █████ ██████████████  
-   █████████ ██████████ █████████ █████ █████ ████ █████  
- ███████████ ███    ███ █████████ █████ █████ ████ █████ 
-██████  █████████████████████ ████ █████ █████ ████ ██████
-]],
-        },
-        -- item field formatters
-        -- formats = {
-        --   icon = function(item)
-        --     if item.file and item.icon == "file" or item.icon == "directory" then
-        --       return M.icon(item.file, item.icon)
-        --     end
-        --     return { item.icon, width = 2, hl = "icon" }
-        --   end,
-        --   footer = { "%s", align = "center" },
-        --   header = { "%s", align = "center" },
-        --   file = function(item, ctx)
-        --     local fname = vim.fn.fnamemodify(item.file, ":~")
-        --     fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
-        --     local dir, file = fname:match("^(.*)/(.+)$")
-        --     return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } } or { { fname, hl = "file" } }
-        --   end,
-        -- },
-        sections = require("utils.dashboard_layout"),
-      },
     }
 
     return vim.tbl_deep_extend("force", opts, config)
