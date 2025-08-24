2aa389d59eb07ea2652c37c9c3f8b58bad16214d
manhhn01 <manhhn01@gmail.com>
2025-08-24 23:32:23 +0700

    feat(git): add neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -94,4 +94,13 @@ return {
       },
     },
   },
+
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+      "folke/snacks.nvim", -- optional
+    },
+  },
 }
