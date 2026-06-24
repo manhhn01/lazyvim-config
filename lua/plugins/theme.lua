c6fcdc665cd6402440f7a7dad03c27afd515f054
manhhn01 <manhhn01@gmail.com>
2026-06-24 16:01:08 +0700

    fix: fix

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,6 +3,7 @@ return {
     "catppuccin/nvim",
     lazy = false,
     name = "catppuccin",
+    priority = 1000,
     config = function(_, opts)
       require("catppuccin").setup(opts)
       vim.cmd.colorscheme("catppuccin")
